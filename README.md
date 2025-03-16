9935e6d2c2d45d567e3e29c6d7c95f66ed892c27
JoelGuerra14 <139911492+JoelGuerra14@users.noreply.github.com>
2025-03-16 18:53:23 -0400

    Cambios para realizar el challenge

---
+++ README.md
@@ -0,0 +1,6 @@
+Challenge Amigo Secreto Alura Latam
+
+Digite un nombre
+Presione el boton "Añadir"
+
+Cuando haya agregado suficientes nombres presione  el boton "Sortear Amigo"
--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+
+   let amigo = document.getElementById("amigo").value;
+   
+   if (amigo == "") {
+        alert("Por favor, inserte un nombre");
+   } else {
+        amigos.push(amigo);
+        limpiarCaja();
+        actualizarLista();
+   }
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let aleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoAleatorio = amigos[aleatorio];
+        document.getElementById("resultado").innerHTML = `El amigo secreto sorteado es ${amigoAleatorio}`;
+    }
+}
+
+function actualizarLista () {
+    let elementoLista = document.getElementById("listaAmigos");
+    elementoLista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        elementoLista.appendChild(li);
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = "";
+}
